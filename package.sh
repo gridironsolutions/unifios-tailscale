2dc2a16dafc1d4d1295d2cae33231fc5131687a0
Tom Voss <tom.voss@gridironsolutions.com>
2022-10-31 14:26:17 -0500

    only include sample .env file

--- package.sh
+++ package.sh
@@ -10,7 +10,8 @@ mkdir -p "${TMP}/unifios-tailscale"
 cp "${1}/ip-rule-monitor.sh" "${TMP}/unifios-tailscale/ip-rule-monitor.sh"
 cp "${1}/10-unifios-tailscale.sh" "${TMP}/unifios-tailscale/10-unifios-tailscale.sh"
 cp "${1}/unifios-tailscale.sh" "${TMP}/unifios-tailscale/unifios-tailscale.sh"
-cp "${1}/.env" "${TMP}/unifios-tailscale/.env"
+cp "${1}/wan-failover-monitor.sh" "${TMP}/unifios-tailscale/wan-failover-monitor.sh"
+cp "${1}/.env.sample" "${TMP}/unifios-tailscale/.env.sample"
 
 mkdir -p "${2}"
 tar czf "${2}/unifios-tailscale.tgz" -C "${TMP}" unifios-tailscale
