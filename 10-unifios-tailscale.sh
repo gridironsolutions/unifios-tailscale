b97673235f9d91b566955445ac50b43c7ca96bdf
Tom Voss <tom.voss@gridironsolutions.com>
2023-03-01 13:32:59 -0600

    compatibility with UniFi OS 2+

--- 10-unifios-tailscale.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-UNIFIOS_TAILSCALE_ROOT="/mnt/data/unifios-tailscale"
-
-${UNIFIOS_TAILSCALE_ROOT}/unifios-tailscale.sh autostart
--- package.sh
+++ package.sh
@@ -8,7 +8,6 @@ trap 'rm -rf ${TMP}' EXIT
 
 mkdir -p "${TMP}/unifios-tailscale"
 cp "${1}/ip-rule-monitor.sh" "${TMP}/unifios-tailscale/ip-rule-monitor.sh"
-cp "${1}/10-unifios-tailscale.sh" "${TMP}/unifios-tailscale/10-unifios-tailscale.sh"
 cp "${1}/unifios-tailscale.sh" "${TMP}/unifios-tailscale/unifios-tailscale.sh"
 cp "${1}/wan-failover-monitor.sh" "${TMP}/unifios-tailscale/wan-failover-monitor.sh"
 cp "${1}/.env.sample" "${TMP}/unifios-tailscale/.env.sample"
